1570fc12e76798cafbd7908082dc7bd5de6e8af3
Eyegff <158478227+Eyegff@users.noreply.github.com>
2024-11-13 17:35:36 +0700

    Update api.py

--- api.py
+++ api.py
@@ -7,15 +7,9 @@ app = Flask(__name__)
 def fetch_code():
     url = "https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/Sub4.txt"
     try:
-        # ดึงข้อมูลจาก URL
         response = requests.get(url)
-        response.raise_for_status()  # ตรวจสอบข้อผิดพลาด
-        data = response.text  # อ่านข้อมูลเป็นข้อความ
+        response.raise_for_status()
+        data = response.text
         return jsonify({"status": "success", "data": data})
     except requests.exceptions.RequestException as e:
-        # จัดการข้อผิดพลาด
         return jsonify({"status": "error", "message": str(e)}), 500
-
-if __name__ == "__main__":
-    # รันแอปพลิเคชัน
-    app.run(host="0.0.0.0", port=5000)
